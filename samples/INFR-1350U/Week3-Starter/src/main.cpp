92c5319b88c679088dc789791359aea86397f78d
DESKTOP-2HJC29U\Hossain Alinaqi <hossain.alinaqi@ontariotechu.net>
2020-09-25 17:22:49 -0400

    final changes

--- samples/INFR-1350U/Week3-Starter/src/main.cpp
+++ samples/INFR-1350U/Week3-Starter/src/main.cpp
@@ -67,7 +67,7 @@ bool initGLFW() {
 	}
 
 	//Create a new GLFW window
-	window = glfwCreateWindow(800, 800, "INFR1350U", nullptr, nullptr);
+	window = glfwCreateWindow(800, 800, "Hossain Alinaqi 100761694", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
 	return true;
